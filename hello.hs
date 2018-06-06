68c3ee5bb59670a7b3c37e76e0cda83ac086d52a
Henry Lancelle <hydro666@gmail.com>
2018-06-05 20:14:51 -0700

    Initial commit.

---
+++ hello.hs
@@ -0,0 +1,15 @@
+data Point = Point Int Int deriving Show
+
+myPoint :: Point
+myPoint = Point 3 4
+
+hypotenuse :: Point -> Int
+hypotenuse point = case point of 
+                    Point x y -> (x^2 + y^2)
+
+data MyPoly a b = MyLeft a | MyRight b | MyAll a b deriving (Show)
+
+weirdList :: [MyPoly Bool Int]
+weirdFirst = MyAll True 5
+weirdSecond = MyLeft False
+weirdList = [weirdFirst, weirdSecond]
---
+++ rockPaperScissors.hs
@@ -0,0 +1,20 @@
+data Move = Rock | Paper | Scissors
+    deriving (Eq, Read, Show, Enum, Bounded)
+
+data Outcome = Lose | Tie | Win deriving (Show, Eq, Ord)
+
+outcome :: Move -> Move -> Outcome
+
+outcome mine opp 
+  | mine == opp = Tie
+  | (mine == Rock) && (opp == Scissors) = Win
+  | (mine == Scissors) && (opp == Rock) = Lose
+  | ((fromEnum mine) - (fromEnum opp)) > 0 = Win
+  | otherwise = Lose
+
+moves = [Rock ..]
+
+combs :: [(Move, Move)]
+combs = [(a, b) | a <- moves, b <- moves]
+
+toShow = zip combs [outcome (fst x) (snd x) | x <- combs]
